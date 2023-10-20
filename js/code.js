48850602206dbbe22dd0bd96466eeb5568e232e6
jesus <jesusadrianmontoya2041@gmail.com>
2023-10-20 13:16:50 -0400

    v3.0; cambio de fondo > color de letra > y de alert
    :wq

--- js/code.js
+++ js/code.js
@@ -42,47 +42,47 @@ if (operación == 1) {
     let numero1 = prompt("primer número para sumar");
     let numero2 = prompt("segundo número para sumar");
     resultado = calculadora.sumar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 2) {
     let numero1 = prompt("primer número para restar");
     let numero2 = prompt("segundo número para restar");
     resultado = calculadora.restar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 3) {
     let numero1 = prompt("primer número para multiplicar");
     let numero2 = prompt("segundo número para multiplicar");
     resultado = calculadora.multiplicar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 4) {
     let numero1 = prompt("primer número para dividir");
     let numero2 = prompt("segundo número para dividir");
     resultado = calculadora.dividir(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 5) {
     let numero1 = prompt("número para potenciar");
     let numero2 = prompt("exponente");
     resultado = calculadora.potenciar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 6) {
     let numero1 = prompt("conocer la raíz cuadrada de:");
     resultado = calculadora.raizCuadrara(numero1);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else if (operación == 7) {
     let numero1 = prompt("conocer la raíz cúbica de:");
     resultado = calculadora.raizCubica(numero1);
-    alert(`tu resultado es ${resultado}`);
+    document.write(`tu resultado es ${resultado}`);
 }
 
 else {
