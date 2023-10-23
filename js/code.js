fb204d8c759dee32facbd9a136c67f0a3c91049a
jesus <jesusadrianmontoya2041@gmail.com>
2023-10-23 12:45:32 -0400

    prompt y saludo javascrit agregados

--- js/code.js
+++ js/code.js
@@ -1,10 +1,18 @@
 
-alert("bienvenido a la pagina de pruebas");
 
+function saludar() {
+    var nombre = prompt("Por favor, ingresa tu nombre");
+    alert("¡Hola " + nombre + "! Bienvenid@");
+  }
+  
+  saludar();
 
-
-
-
+  alert("dale click al boton")
+ 
+  function bienvenida() {
+    alert(`espero te haya gustado`);
+  }
+  
 
 
 
